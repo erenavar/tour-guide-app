f1018f86d55f62c1a113290409ee0eb020b0918e
Eren AVAR <eren.avar@hotmail.com>
2025-05-18 16:34:05 +0300

    create navigation structure and home screen

---
+++ src/Navigation/MainNavigation.tsx
@@ -0,0 +1,18 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "./types";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function MainNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainNavigation;
