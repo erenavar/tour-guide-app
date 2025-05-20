3a47ca96e5aa6a6542135a21a3a53e1de8c5c1a2
Eren AVAR <eren.avar@hotmail.com>
2025-05-20 21:58:51 +0300

    complete header css and content

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,31 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>Hello World</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Ionicons name="menu" size={30} color="black" />
+        <Text style={styles.headerText}>Tour Guide App</Text>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: "5%",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 19,
+  },
+});
 
 export default HomeScreen;
