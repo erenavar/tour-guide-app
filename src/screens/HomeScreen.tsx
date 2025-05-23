d0f6ad2340360d0239561d006a2de6367dd7c4f0
Eren AVAR <eren.avar@hotmail.com>
2025-05-24 00:21:06 +0300

    adjust texts on home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,11 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 const HomeScreen = () => {
@@ -8,13 +15,22 @@ const HomeScreen = () => {
         <Ionicons name="menu" size={30} color="black" />
         <Text style={styles.headerText}>Tour Guide App</Text>
       </View>
+      <View style={{ marginTop: 30 }}>
+        <Text style={styles.findText}>Find Your Trip</Text>
+      </View>
+      <View style={styles.contentHeader}>
+        <Text style={styles.trips}>Popular Trips</Text>
+        <Text style={styles.seeAll}>See All</Text>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: "5%",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    marginHorizontal: "4%",
+    marginTop: "5%",
   },
   header: {
     display: "flex",
@@ -25,6 +41,24 @@ const styles = StyleSheet.create({
   headerText: {
     fontWeight: "bold",
     fontSize: 19,
+  },
+  findText: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  contentHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "20%",
+    alignItems: "center",
+  },
+  trips: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  seeAll: {
+    color: "dodgerblue",
+    fontSize: 17,
   },
 });
 
